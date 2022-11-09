40fcf21172dcc9d3505c75bd0b467d414cd913f5
Embedded-Sp3zz <brianmhinger@gmail.com>
2022-11-09 15:04:39 +0000

    Set gpio mode and direction in build

--- build.sh
+++ build.sh
@@ -13,6 +13,10 @@ echo "Configuring GPIO pin p9.12 to gpio (output) and p8.16 to gpio_pu (input)"
 
 config-pin p9.12 gpio
 
+echo out > /sys/class/gpio/gpio60/direction
+
 config-pin p8.16 gpio_pu
+
+echo in > /sys/class/gpio/gpio46/direction
 
 echo "Build complete"
